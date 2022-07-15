806176e8acc494fbd72db1450c814eabdc59eca8
TiagoSeverino <tiago@severino.xyz>
2022-07-15 19:47:22 +0100

    Fix Unknown Logo

--- src/MatchResult/TeamLogo.tsx
+++ src/MatchResult/TeamLogo.tsx
@@ -1,9 +1,18 @@
 import {Img} from 'remotion';
 
-export const TeamLogo = ({url, team2 = false}) => {
+export const TeamLogo = ({
+	url,
+	team2 = false,
+}: {
+	url: string;
+	team2?: boolean;
+}) => {
 	return (
 		<div className={`team${team2 ? '2' : '1'}`}>
-			<Img className="logo" src={url} />
+			<Img
+				className="logo"
+				src={`${url.startsWith('/') ? 'https://www.hltv.org' : ''}${url}`}
+			/>
 		</div>
 	);
 };
