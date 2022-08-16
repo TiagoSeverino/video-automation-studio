a73f224a2ec3bd1d8ce28ca43264a30a6fd930f1
TiagoSeverino <tiago@severino.xyz>
2022-08-16 23:31:41 +0100

    Add Image Search

---
+++ src/google/search.ts
@@ -0,0 +1,19 @@
+import {google} from 'googleapis';
+
+const customSearch = google.customsearch('v1');
+
+export const searchImages = async (query: string, total = 2) => {
+	const response = await customSearch.cse.list({
+		auth: process.env.GOOGLE_SEARCH_API_KEY,
+		cx: process.env.GOOGLE_SEARCH_API_ENGINE_ID,
+		q: query,
+		searchType: 'image',
+		num: 3,
+	});
+
+	if (!response.data.items) return [];
+
+	return response.data.items.map((item) => {
+		return item.link;
+	});
+};
