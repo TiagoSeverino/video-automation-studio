d89973a730dd80e56c0f4a8a30cf86ca042eb70d
TiagoSeverino <tiago@severino.xyz>
2022-08-16 23:37:56 +0100

    Fix num

--- src/google/search.ts
+++ src/google/search.ts
@@ -2,13 +2,13 @@ import {google} from 'googleapis';
 
 const customSearch = google.customsearch('v1');
 
-export const searchImages = async (query: string, total = 2) => {
+export const searchImages = async (query: string, num = 2) => {
 	const response = await customSearch.cse.list({
 		auth: process.env.GOOGLE_SEARCH_API_KEY,
 		cx: process.env.GOOGLE_SEARCH_API_ENGINE_ID,
 		q: query,
 		searchType: 'image',
-		num: 3,
+		num,
 	});
 
 	if (!response.data.items) return [];
