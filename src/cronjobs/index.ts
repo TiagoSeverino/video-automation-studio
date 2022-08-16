664c26dcbb49940be0b9a67b1a6a92b160d906cb
TiagoSeverino <tiago@severino.xyz>
2022-08-16 20:46:34 +0100

    Fetch matches cronjob

--- src/cronjobs/index.ts
+++ src/cronjobs/index.ts
@@ -1,6 +1,14 @@
 import cron from 'node-cron';
+import {availableGames, getMatches} from '../esports';
 
 cron.schedule('*/15 * * * *', () => {
 	console.log(new Date().toUTCString());
-	console.log('running a task every 15 minutes');
+
+	availableGames.map(async (game) => {
+		const matches = await getMatches(game);
+
+		if (matches.length > 0) {
+			console.log(`${matches.length} matches found for ${game}`);
+		}
+	});
 });
