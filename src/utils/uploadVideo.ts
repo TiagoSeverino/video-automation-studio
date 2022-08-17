7906f4d6ec475901daf641fb77f0acb62399d088
TiagoSeverino <tiago@severino.xyz>
2022-08-17 20:53:13 +0100

    Fix crash on upload error

--- src/utils/uploadVideo.ts
+++ src/utils/uploadVideo.ts
@@ -1,23 +1,28 @@
 import uploadYoutube from '../apis/google/youtube';
-import log from '../apis/log';
+import log, {logError} from '../apis/log';
 import getYoutubeCredentials from './getYoutubeCredentials';
 
 export default async (videoData: VideoData) => {
-	const youtubeCredentials = await getYoutubeCredentials();
+	try {
+		const youtubeCredentials = await getYoutubeCredentials();
 
-	if (youtubeCredentials) {
-		videoData = await uploadYoutube(
-			videoData,
-			youtubeCredentials,
-			youtubeCredentials.tokens[0]
-		);
-		log(
-			`${videoData.title} - https://youtu.be/${
-				videoData.platforms!.youtube!.id
-			}`
-		);
+		if (youtubeCredentials) {
+			videoData = await uploadYoutube(
+				videoData,
+				youtubeCredentials,
+				youtubeCredentials.tokens[0]
+			);
+			log(
+				`${videoData.title} - https://youtu.be/${
+					videoData.platforms!.youtube!.id
+				}`
+			);
 
-		return true;
+			return true;
+		}
+	} catch (e) {
+		console.error(e);
+		logError('Error uploading video to youtube');
 	}
 
 	return false;
