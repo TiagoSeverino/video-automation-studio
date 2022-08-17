cc49bb9c3a1298497724c2684d28fd2d5e4aa624
TiagoSeverino <tiago@severino.xyz>
2022-08-17 01:08:33 +0100

    Add TimeStamps

--- src/database/models/MatchResult.ts
+++ src/database/models/MatchResult.ts
@@ -6,12 +6,17 @@ const team = {
 	rounds: {type: Number, required: true},
 };
 
-const matchResultSchema = new Schema<MatchResult>({
-	id: {type: String, required: true},
-	team1: team,
-	team2: team,
-	tournament: {type: String, required: false},
-	stars: {type: Number, required: true},
-});
+const matchResultSchema = new Schema<MatchResult>(
+	{
+		id: {type: String, required: true},
+		team1: team,
+		team2: team,
+		tournament: {type: String, required: false},
+		stars: {type: Number, required: true},
+	},
+	{
+		timestamps: true,
+	}
+);
 
 export default model<MatchResult>('MatchResult', matchResultSchema);
