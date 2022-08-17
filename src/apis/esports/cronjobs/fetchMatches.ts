ead1ee37709738ddfd931af082b2b0cd78e00467
TiagoSeverino <tiago@severino.xyz>
2022-08-18 00:21:07 +0100

    Update fetchMatches

--- src/apis/esports/cronjobs/fetchMatches.ts
+++ src/apis/esports/cronjobs/fetchMatches.ts
@@ -1,8 +1,30 @@
 import {availableESports, getMatches} from '..';
+import MatchResult from '../../../database/models/MatchResult';
 import log from '../../log';
 
 export default () =>
 	availableESports.map(async (game) => {
+		//Find matches for the game
 		const matches = await getMatches(game);
-		log(`${matches.length} matches found for ${game}`);
+
+		const before = (await MatchResult.find({game})).length;
+
+		//Store matches in the database
+		await Promise.all(
+			matches.map(async (match) => {
+				await MatchResult.updateOne(
+					{
+						id: match.id,
+					},
+					{...match, game},
+					{
+						upsert: true,
+					}
+				);
+			})
+		);
+
+		const after = (await MatchResult.find({game})).length;
+
+		if (after > before) log(`${after - before} matches found for ${game}`);
 	});
