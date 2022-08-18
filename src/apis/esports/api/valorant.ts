130724cc91279c1f5c0d3d23d211094a7945b731
TiagoSeverino <tiago@severino.xyz>
2022-08-18 16:25:31 +0100

    Fix Valorant Image URLs

--- src/apis/esports/api/valorant.ts
+++ src/apis/esports/api/valorant.ts
@@ -2,7 +2,11 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 const fixLogoUrl = (url: string) =>
-	url.startsWith('//') ? `https:${url}` : url;
+	url.startsWith('/')
+		? url.startsWith('//')
+			? `https:${url}`
+			: `https://www.vlr.gg/${url}`
+		: url;
 
 export default async (): Promise<MatchResult[]> => {
 	const res = await axios.get('https://www.vlr.gg/matches/results');
