f2342b8486177a77306e19fcdf659ca731ea21a3
TiagoSeverino <tiago@severino.xyz>
2022-09-02 22:06:18 +0100

    Use @TiagoSeverino/youtube-uploader

--- src/apis/esports/cronjobs/uploadVideo.ts
+++ src/apis/esports/cronjobs/uploadVideo.ts
@@ -1,46 +1,12 @@
 import ESportsVideoData from '../../../database/models/ESportsVideoData';
 import TiktokAccount from '../../../database/models/TiktokAccount';
 import YoutubeCredentialStorage from '../../../database/models/YoutubeCredentialStorage';
-import uploadYoutube from '../../google/youtube';
 import log, {logError} from '../../log';
 import uploadTiktok from '@TiagoSeverino/tiktok-uploader';
+import uploadYoutube from '@TiagoSeverino/youtube-uploader';
+import {getYoutubeChannelName} from '..';
 
 export default async () => {
-	const videosForYoutube = (await ESportsVideoData.find()).filter(
-		(videoData) => !videoData.platforms?.youtube?.id
-	);
-
-	if (videosForYoutube.length > 0) {
-		const youtubeCredentials = await YoutubeCredentialStorage.findOne();
-		if (!youtubeCredentials)
-			return logError('No youtube credentials found');
-
-		for (const videoData of videosForYoutube) {
-			try {
-				const youtubeId = await uploadYoutube(
-					videoData,
-					youtubeCredentials,
-					youtubeCredentials.tokens[0]
-				);
-
-				videoData.platforms = {
-					...videoData.platforms,
-					youtube: {
-						id: youtubeId,
-					},
-				};
-
-				await videoData.save();
-
-				log(`https://youtu.be/${youtubeId}`);
-			} catch (err) {
-				logError("Couldn't upload video to youtube");
-				console.error(err);
-				break;
-			}
-		}
-	}
-
 	const videosForTiktok = (await ESportsVideoData.find()).filter(
 		(videoData) => !videoData.platforms?.tiktok?.id
 	);
@@ -79,6 +45,60 @@ export default async () => {
 				console.error(err);
 
 				break;
+			}
+		}
+	}
+
+	const videosForYoutube = (await ESportsVideoData.find()).filter(
+		(videoData) => !videoData.platforms?.youtube?.id
+	);
+
+	if (videosForYoutube.length > 0) {
+		const youtubeCredentials = await YoutubeCredentialStorage.findOne();
+		if (!youtubeCredentials)
+			return logError('No youtube credentials found');
+
+		for (const videoData of videosForYoutube) {
+			try {
+				const {path, title, description, tags, game} = videoData;
+
+				const channelName = getYoutubeChannelName(game);
+
+				const url = await uploadYoutube(
+					{
+						path,
+						title,
+						description: `${description}
+
+Created by Tiago Severino
+Github - https://github.com/TiagoSeverino
+Linkedin - https://www.linkedin.com/in/tiagoseverino/`,
+						tags,
+						channelName,
+					},
+					require('../../../../yt-auth/cookies.json')
+				);
+
+				if (!url) continue;
+
+				const id = url.split('/').pop();
+
+				if (!id) return log(`ID Not found for youtube: ${url}`);
+
+				videoData.platforms = {
+					...videoData.platforms,
+					youtube: {
+						id,
+					},
+				};
+
+				videoData
+					.save()
+					.then(() =>
+						log(`${videoData.title} - https://youtu.be/${id}`)
+					);
+			} catch (e) {
+				logError(`Failed to upload to youtube: ${e}`);
 			}
 		}
 	}
